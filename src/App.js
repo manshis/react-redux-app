c6c0f9c8c956ecbd06b7aa80a4c3d4ade518aba5
= <=>
2020-04-28 11:24:00 +0530

    Async writes in redux

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, Switch } from 'react-router-dom';
 import Header from './components/common/Header';
 import PageNotFound from './components/PageNotFound';
 import CoursesPage from './components/courses/CoursesPage';
+import ManageCoursePage from './components/courses/ManageCoursePage';
 
 
 function App() {
@@ -16,6 +17,8 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/courses" component={CoursesPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
         <Route component={PageNotFound} />
       </Switch>
 
